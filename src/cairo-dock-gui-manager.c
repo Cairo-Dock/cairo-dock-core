a4297c03742608999ee05f4ee5c1bb33a81c3981
Fabounet (Fabrice Rey) - http://cairo-dock.org <>
2009-11-14 03:47:57 +0100

    changed some icons in the config panel

--- src/cairo-dock-gui-manager.c
+++ src/cairo-dock-gui-manager.c
@@ -101,7 +101,7 @@ extern gboolean g_bEasterEggs;
 const gchar *cCategoriesDescription[2*CAIRO_DOCK_NB_CATEGORY] = {
 	N_("Behaviour"), CAIRO_DOCK_SHARE_DATA_DIR"/icon-behavior.svg",
 	N_("Appearance"), CAIRO_DOCK_SHARE_DATA_DIR"/icon-appearance.svg",
-	N_("Accessories"), CAIRO_DOCK_SHARE_DATA_DIR"/icon-accessories.svg",
+	N_("Accessories"), CAIRO_DOCK_SHARE_DATA_DIR"/icon-accessories.png",
 	N_("Desktop"), CAIRO_DOCK_SHARE_DATA_DIR"/icon-desktop.svg",
 	N_("Controlers"), CAIRO_DOCK_SHARE_DATA_DIR"/icon-controler.svg",
 	N_("Plug-ins"), "gtk-disconnect" };
