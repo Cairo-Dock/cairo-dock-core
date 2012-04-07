0ef74cd898d12b093928b83ef2a721b01072eba1
Matthieu Baerts <matttbe@gmail.com>
2012-04-07 02:41:39 +0200

    initial-setup: no longer modify global gconf keys of gnome settings  + Check if DBus Compiz's plugin is enabled even if Compiz uses default settings (this plug-in is required to communicate with Compiz)

--- data/scripts/initial-setup.sh
+++ data/scripts/initial-setup.sh
@@ -19,19 +19,12 @@
 
 # Note: we have to use $HOME instead of '~'
 
-# Enable icons in menus and buttons in Gnome
-if test -n "`which gsettings`"; then
-	gsettings set org.gnome.desktop.interface buttons-have-icons true 2> /dev/null
-	gsettings set org.gnome.desktop.interface menus-have-icons true 2> /dev/null
-fi
-if test -n "`which gconftool-2`"; then  # both gsettings and gconf can be present on the system, so we do both.
-	GCONFTOOL_CHECK=1
-	gconftool-2 -s '/desktop/gnome/interface/buttons_have_icons' --type bool true
-	gconftool-2 -s '/desktop/gnome/interface/menus_have_icons' --type bool true
-fi
-
 # Enable dbus, scale and expo plug-ins in Compiz.
 if test -n "`which compiz`"; then
+	if test -n "`which gconftool-2`"; then
+		GCONFTOOL_CHECK=1
+	fi
+
 	if test -d "$HOME/.config/compiz"; then # compiz < 0.9
 		# flat file
 		if test -f "$HOME/.config/compiz/compizconfig/Default.ini"; then
@@ -58,12 +51,17 @@ if test -n "`which compiz`"; then
 		# gconf
 		if test -n "$GCONFTOOL_CHECK"; then
 			plugins=`gconftool-2 -g /apps/compiz-1/general/screen0/options/active_plugins`
-			for i in dbus scale expo; do
-				if test `echo $plugins | grep -c $i` -eq 0; then
-					plugins=${plugins:0:${#plugins}-1},$i]
-					gconftool-2 -s /apps/compiz-1/general/screen0/options/active_plugins --type=list --list-type=string "$plugins"
-				fi
-			done
+			if test -n "$plugins"; then
+				for i in dbus scale expo; do
+					if test `echo $plugins | grep -c $i` -eq 0; then
+						plugins=${plugins:0:${#plugins}-1},$i]
+						gconftool-2 -s /apps/compiz-1/general/screen0/options/active_plugins --type=list --list-type=string "$plugins"
+					fi
+				done
+			else # it's possible that the gconf is empty
+				plugins="[core,composite,opengl,compiztoolbox,decor,vpswitch,snap,mousepoll,resize,place,move,wall,grid,regex,imgpng,session,gnomecompat,animation,fade,staticswitcher,workarounds,scale,expo,ezoom,dbus]"
+				gconftool-2 -s /apps/compiz-1/general/screen0/options/active_plugins --type=list --list-type=string "$plugins"
+			fi
 		fi
 	fi
 fi
