3e49f7f79e481232db019ac5efe36b3d145d1bbe
Fabrice Rey <fabounet03@gmail.com>
2012-05-03 01:10:05 +0200

    added a compilation option to not force the display of icons in menus

--- src/cairo-dock-menu.c
+++ src/cairo-dock-menu.c
@@ -470,10 +470,7 @@ GtkWidget *_add_item_sub_menu (Icon *icon, GtkWidget *pMenu)
 		GtkWidget *pMenuItem = gtk_image_menu_item_new_with_label (cName);
 		GtkWidget *image = gtk_image_new_from_pixbuf (pixbuf);
 		g_object_unref (pixbuf);
-		#if (GTK_MAJOR_VERSION > 2 || GTK_MINOR_VERSION >= 16)
-		gtk_image_menu_item_set_always_show_image (GTK_IMAGE_MENU_ITEM (pMenuItem), TRUE);
-		#endif
-		gtk_image_menu_item_set_image (GTK_IMAGE_MENU_ITEM (pMenuItem), image);
+		_gtk_image_menu_item_set_image (GTK_IMAGE_MENU_ITEM (pMenuItem), image);
 		
 		gtk_menu_shell_append (GTK_MENU_SHELL (pMenu), pMenuItem); 
 		
