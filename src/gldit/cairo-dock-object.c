8afcfc00c3be47da0c0c0e9d4d542f5f839549ab
Fabrice Rey <fabounet03@gmail.com>
2013-05-19 00:31:55 +0200

    Improved the Object class, merged with notifications.c; from now any object in the core will be based on it

--- src/gldit/cairo-dock-object.c
+++ src/gldit/cairo-dock-object.c
@@ -18,16 +18,144 @@
 */
 
 #include "cairo-dock-struct.h"
-#include "cairo-dock-notifications.h"
 #include "cairo-dock-manager.h"
+#include "cairo-dock-log.h"
 #include "cairo-dock-object.h"
 
-/* new_dialog -> new_object (mgr, attr) -> mgr->top_parent->init(attr) -> mgr->parent->init(attr) -> mgr->init(attr) --> notif
+/* obj -> mgr0 -> mgr1 -> ... -> mgrN
+ * new_dialog -> new_object (mgr, attr) -> mgr->top_parent->init(attr) -> mgr->parent->init(attr) -> mgr->init(attr) --> notif
  * unref_object -> ref-- -> notif -> mgr->destroy -> mgr->parent->destroy -> mgr->top_parent->destroy --> free
+ * GLDI_OBJECT_IS_xxx obj->mgr == pMgr || mgr->parent->mrg == pMgr || ...
  * */
 
 void gldi_object_set_manager (GldiObject *pObject, GldiManager *pMgr)
 {
 	pObject->mgr = pMgr;
-	cairo_dock_install_notifications_on_object (pObject, pMgr->object.pNotificationsTab->len);
+	pObject->mgrs = g_list_copy (pMgr->object.mgrs);
+	pObject->mgrs = g_list_append (pObject->mgrs, pMgr);
+	gldi_object_install_notifications (pObject, pMgr->object.pNotificationsTab->len);
+}
+void gldi_object_init (GldiObject *obj, GldiManager *pMgr, gpointer attr)
+{
+	obj->ref = 1;
+	// set the manager
+	gldi_object_set_manager (obj, pMgr);
+	
+	// init the object
+	GList *m;
+	for (m = obj->mgrs; m != NULL; m = m->next)
+	{
+		pMgr = m->data;
+		if (pMgr->init_object)
+			pMgr->init_object (obj, attr);
+	}
+	// emit a notification
+	gldi_object_notify (obj, NOTIFICATION_NEW, obj);
+}
+
+GldiObject *gldi_object_new (GldiManager *pMgr, gpointer attr)
+{
+	GldiObject *obj = g_malloc0 (pMgr->iObjectSize);
+	gldi_object_init (obj, pMgr, attr);
+	return obj;
+}
+
+void gldi_object_ref (GldiObject *pObject)
+{
+	g_return_if_fail (pObject != NULL && pObject->ref > 0);
+	pObject->ref ++;
+}
+
+void gldi_object_unref (GldiObject *pObject)
+{
+	if (pObject == NULL)
+		return;
+	pObject->ref --;
+	if (pObject->ref == 0)  // so if it was already 0, don't do anything
+	{
+		// emit a notification
+		gldi_object_notify (pObject, NOTIFICATION_DESTROY, pObject);
+		
+		// reset the object
+		GldiManager *pMgr = pObject->mgr;
+		while (pMgr)
+		{
+			if (pMgr->reset_object)
+				pMgr->reset_object (pObject);
+			pMgr = pMgr->object.mgr;
+		}
+		
+		// clear notifications
+		GPtrArray *pNotificationsTab = pObject->pNotificationsTab;
+		guint i;
+		for (i = 0; i < pNotificationsTab->len; i ++)
+		{
+			GSList *pNotificationRecordList = g_ptr_array_index (pNotificationsTab, i);
+			g_slist_foreach (pNotificationRecordList, (GFunc)g_free, NULL);
+			g_slist_free (pNotificationRecordList);
+		}
+		g_ptr_array_free (pNotificationsTab, TRUE);
+		
+		// free memory
+		g_free (pObject);
+	}
+}
+
+
+gboolean gldi_object_is_manager_child (GldiObject *pObject, GldiManager *pMgr)
+{
+	while (pObject)
+	{
+		if (pObject->mgr == pMgr)
+			return TRUE;
+		pObject = GLDI_OBJECT (pObject->mgr);
+	}
+	return FALSE;
+}
+
+#define GLDI_OBJECT_IS_DOCK(obj) gldi_object_is_manager_child (obj, &myDocksMgr)
+
+
+
+void gldi_object_register_notification (gpointer pObject, GldiNotificationType iNotifType, GldiNotificationFunc pFunction, gboolean bRunFirst, gpointer pUserData)
+{
+	g_return_if_fail (pObject != NULL);
+	// grab the notifications tab
+	GPtrArray *pNotificationsTab = GLDI_OBJECT(pObject)->pNotificationsTab;
+	if (!pNotificationsTab || pNotificationsTab->len < iNotifType)
+	{
+		cd_warning ("someone tried to register to an inexisting notification (%d) on an object of type '%s'", iNotifType, GLDI_OBJECT(pObject)->mgr?GLDI_OBJECT(pObject)->mgr->cModuleName:"manager");
+		return ;  // don't try to create/resize the notifications tab, since noone will emit this notification.
+	}
+	
+	// add a record
+	GldiNotificationRecord *pNotificationRecord = g_new (GldiNotificationRecord, 1);
+	pNotificationRecord->pFunction = pFunction;
+	pNotificationRecord->pUserData = pUserData;
+	
+	GSList *pNotificationRecordList = g_ptr_array_index (pNotificationsTab, iNotifType);
+	pNotificationsTab->pdata[iNotifType] = (bRunFirst ? g_slist_prepend : g_slist_append) (pNotificationRecordList, pNotificationRecord);
+}
+
+
+void gldi_object_remove_notification (gpointer pObject, GldiNotificationType iNotifType, GldiNotificationFunc pFunction, gpointer pUserData)
+{
+	g_return_if_fail (pObject != NULL);
+	// grab the notifications tab
+	GPtrArray *pNotificationsTab = GLDI_OBJECT(pObject)->pNotificationsTab;
+	
+	// remove the record
+	GSList *pNotificationRecordList = g_ptr_array_index (pNotificationsTab, iNotifType);
+	GldiNotificationRecord *pNotificationRecord;
+	GSList *nr;
+	for (nr = pNotificationRecordList; nr != NULL; nr = nr->next)
+	{
+		pNotificationRecord = nr->data;
+		if (pNotificationRecord->pFunction == pFunction && pNotificationRecord->pUserData == pUserData)
+		{
+			pNotificationsTab->pdata[iNotifType] = g_slist_delete_link (pNotificationRecordList, nr);
+			g_free (pNotificationRecord);
+			break;
+		}
+	}
 }
--- src/gldit/cairo-dock-object.h
+++ src/gldit/cairo-dock-object.h
@@ -33,25 +33,117 @@ struct _GldiObject {
 	gint ref;
 	GPtrArray *pNotificationsTab;
 	GldiManager *mgr;
+	GList *mgrs;  // sorted in reverse order
 };
 
 /// signals
 typedef enum {
-	/// notification called when the object is going to be destroyed. data : NULL
+	/// notification called when an object has been created. data : the object
+	NOTIFICATION_NEW,
+	/// notification called when the object is going to be destroyed. data : the object
 	NOTIFICATION_DESTROY,
 	NB_NOTIFICATIONS_OBJECT
-	} CairoObjectNotifications;
+	} GldiObjectNotifications;
 
 #define GLDI_OBJECT(p) ((GldiObject*)(p))
 
-#define gldi_object_new(Type, mgr) \
-__extension__ ({ \
-	Type *obj = g_new0 (Type, 1); \
-	GLDI_OBJECT(obj)->ref = 1; \
-	gldi_object_set_manager (GLDI_OBJECT (obj), GLDI_MANAGER (mgr)); \
-	obj; })
+
+void gldi_object_init (GldiObject *obj, GldiManager *pMgr, gpointer attr);
+
+GldiObject *gldi_object_new (GldiManager *pMgr, gpointer attr);
+
+void gldi_object_ref (GldiObject *pObject);
+
+void gldi_object_unref (GldiObject *pObject);
+
 
 void gldi_object_set_manager (GldiObject *pObject, GldiManager *pMgr);
+
+gboolean gldi_object_is_manager_child (GldiObject *pObject, GldiManager *pMgr);
+
+
+/// Generic prototype of a notification callback.
+typedef gboolean (* GldiNotificationFunc) (gpointer pUserData, ...);
+
+typedef struct {
+	GldiNotificationFunc pFunction;
+	gpointer pUserData;
+	} GldiNotificationRecord;
+
+typedef guint GldiNotificationType;
+
+/// Use this in \ref gldi_object_register_notification to be called before the dock.
+#define GLDI_RUN_FIRST TRUE
+/// Use this in \ref gldi_object_register_notification to be called after the dock.
+#define GLDI_RUN_AFTER FALSE
+
+/// Return this in your callback to prevent the other callbacks from being called after you.
+#define GLDI_NOTIFICATION_INTERCEPT TRUE
+/// Return this in your callback to let pass the notification to the other callbacks after you.
+#define GLDI_NOTIFICATION_LET_PASS FALSE
+
+
+#define gldi_object_install_notifications(pObject, iNbNotifs) do {\
+	GPtrArray *pNotificationsTab = ((GldiObject*)pObject)->pNotificationsTab;\
+	if (pNotificationsTab == NULL) {\
+		pNotificationsTab = g_ptr_array_new ();\
+		((GldiObject*)pObject)->pNotificationsTab = pNotificationsTab; }\
+	if (pNotificationsTab->len < iNbNotifs)\
+		g_ptr_array_set_size (pNotificationsTab, iNbNotifs); } while (0)
+
+/** Register an action to be called when a given notification is broadcasted from a given object.
+*@param pObject the object (Icon, Container, Manager).
+*@param iNotifType type of the notification.
+*@param pFunction callback.
+*@param bRunFirst CAIRO_DOCK_RUN_FIRST to be called before Cairo-Dock, CAIRO_DOCK_RUN_AFTER to be called after.
+*@param pUserData data to be passed as the first parameter of the callback.
+*/
+void gldi_object_register_notification (gpointer pObject, GldiNotificationType iNotifType, GldiNotificationFunc pFunction, gboolean bRunFirst, gpointer pUserData);
+
+/** Remove a callback from the list of callbacks of a given object for a given notification and a given data.
+Note: it is safe to remove the callback when it is called, but not another one.
+*@param pObject the object (Icon, Container, Manager) for which the action has been registered.
+*@param iNotifType type of the notification.
+*@param pFunction callback.
+*@param pUserData data that was registerd with the callback.
+*/
+void gldi_object_remove_notification (gpointer pObject, GldiNotificationType iNotifType, GldiNotificationFunc pFunction, gpointer pUserData);
+
+
+#define __notify(pNotificationRecordList, bStop, ...) do {\
+	GldiNotificationRecord *pNotificationRecord;\
+	GSList *pElement = pNotificationRecordList, *pNextElement;\
+	while (pElement != NULL && ! bStop) {\
+		pNotificationRecord = pElement->data;\
+		pNextElement = pElement->next;\
+		bStop = pNotificationRecord->pFunction (pNotificationRecord->pUserData, ##__VA_ARGS__);\
+		pElement = pNextElement; }\
+	} while (0)
+
+#define __notify_on_object(pObject, iNotifType, ...) \
+	__extension__ ({\
+	gboolean _stop = FALSE;\
+	GPtrArray *pNotificationsTab = (pObject)->pNotificationsTab;\
+	if (pNotificationsTab && iNotifType < pNotificationsTab->len) {\
+		GSList *pNotificationRecordList = g_ptr_array_index (pNotificationsTab, iNotifType);\
+		__notify (pNotificationRecordList, _stop, ##__VA_ARGS__);} \
+	else {_stop = TRUE;}\
+	_stop; })
+
+/** Broadcast a notification on a given object, and on all its managers.
+*@param pObject the object (Icon, Container, Manager, ...).
+*@param iNotifType type of the notification.
+*@param ... parameters to be passed to the callbacks that have registered to this notification.
+*/
+#define gldi_object_notify(pObject, iNotifType, ...) \
+	__extension__ ({\
+	gboolean _bStop = FALSE;\
+	GldiObject *_obj = GLDI_OBJECT (pObject);\
+	while (_obj && !_bStop) {\
+		_bStop = __notify_on_object (_obj, iNotifType, ##__VA_ARGS__);\
+		_obj = GLDI_OBJECT (_obj->mgr); }\
+	})
+
 
 G_END_DECLS
 #endif
