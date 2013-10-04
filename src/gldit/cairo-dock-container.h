47742d39bc96da6adede136ef00b3d335a1c5cd0
Matthieu Baerts <matttbe@gmail.com>
2013-10-04 20:31:36 +0200

    Container: update mouse position: fixed a possible crash gdk_display_get_device_manager can return NULL (e.g. at startup?)

--- src/gldit/cairo-dock-container.h
+++ src/gldit/cairo-dock-container.h
@@ -231,6 +231,7 @@ void cairo_dock_disable_containers_opacity (void);
 #else
 #define gldi_container_update_mouse_position(pContainer) do {\
 	GdkDeviceManager *pManager = gdk_display_get_device_manager (gtk_widget_get_display (pContainer->pWidget)); \
+	if (pManager == NULL) break; \
 	GdkDevice *pDevice = gdk_device_manager_get_client_pointer (pManager); \
 	if ((pContainer)->bIsHorizontal) \
 		gdk_window_get_device_position (gldi_container_get_gdk_window (pContainer), pDevice, &pContainer->iMouseX, &pContainer->iMouseY, NULL); \
