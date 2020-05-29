dd4f12c45bb9e1368b1b7e5aee525f92c6ccb843
fabounet03 <fabounet03@gmail.com>
2020-05-29 22:46:02 +0900

    Menus: updated the css to work with recent GTK Also draw the separators ourselves, in a consistent way with the menu's line color. Finally, some colors were incorrect when using system colors.

--- src/gldit/cairo-dock-menu.c
+++ src/gldit/cairo-dock-menu.c
@@ -39,12 +39,8 @@
 
 extern gchar *g_cCurrentThemePath;
 
-/*
+
 static gboolean _draw_menu_item (GtkWidget *widget, cairo_t *cr, G_GNUC_UNUSED gpointer data);
-static gboolean _on_select_menu_item (GtkWidget* pMenuItem, G_GNUC_UNUSED gpointer data);
-static gboolean _on_deselect_menu_item (GtkWidget* pMenuItem, G_GNUC_UNUSED gpointer data);
-static void _on_destroy_menu_item (GtkWidget* pMenuItem, G_GNUC_UNUSED gpointer data);
-*/
 
   ////////////
  /// MENU ///
@@ -84,13 +80,20 @@ void _init_menu_style (void)
 		}
 		
 		// css header: define colors from the global style
-		GldiColor *bg_color = (myDialogsParam.bUseDefaultColors ? &myStyleParam.fBgColor : &myDialogsParam.fBgColor);
-		GldiColor *text_color = (myDialogsParam.bUseDefaultColors ? &myStyleParam.textDescription.fColorStart : &myDialogsParam.dialogTextDescription.fColorStart);
-		
+		GldiColor bg_color;
+		if (myDialogsParam.bUseDefaultColors)
+			gldi_style_color_get (GLDI_COLOR_BG, &bg_color);
+		else
+			bg_color = myDialogsParam.fBgColor;
+		GldiColor text_color;
+		if (myDialogsParam.bUseDefaultColors)
+			gldi_style_color_get (GLDI_COLOR_TEXT, &text_color);
+		else
+			text_color = myDialogsParam.dialogTextDescription.fColorStart;
 		GldiColor rgb;  // menuitem bg color: a little darker/lighter than the menu's bg color; also separator color (with no alpha)
-		gldi_style_color_shade (bg_color, GLDI_COLOR_SHADE_MEDIUM, &rgb);
+		gldi_style_color_shade (&bg_color, GLDI_COLOR_SHADE_MEDIUM, &rgb);
 		GldiColor rgbb;  // menuitem border color and menuitem's child bg color (for instance, calendar, scale, etc): a little darker/lighter than the menuitem bg color
-		gldi_style_color_shade (bg_color, GLDI_COLOR_SHADE_STRONG, &rgbb);
+		gldi_style_color_shade (&bg_color, GLDI_COLOR_SHADE_STRONG, &rgbb);
 		
 		gchar *cssheader = g_strdup_printf ("@define-color menuitem_bg_color rgba (%d, %d, %d, %f); \n\
 		@define-color menuitem_text_color rgb (%d, %d, %d); \n\
@@ -99,11 +102,11 @@ void _init_menu_style (void)
 		@define-color menuitem_child_bg_color rgba (%d, %d, %d, %f); \n\
 		@define-color menu_bg_color rgba (%d, %d, %d, %f);\n",
 			(int)(rgb.rgba.red*255), (int)(rgb.rgba.green*255), (int)(rgb.rgba.blue*255), rgb.rgba.alpha,
-			(int)(text_color->rgba.red*255), (int)(text_color->rgba.green*255), (int)(text_color->rgba.blue*255),
-			(int)(text_color->rgba.red*255), (int)(text_color->rgba.green*255), (int)(text_color->rgba.blue*255),
+			(int)(text_color.rgba.red*255), (int)(text_color.rgba.green*255), (int)(text_color.rgba.blue*255),
+			(int)(text_color.rgba.red*255), (int)(text_color.rgba.green*255), (int)(text_color.rgba.blue*255),
 			(int)(rgb.rgba.red*255), (int)(rgb.rgba.green*255), (int)(rgb.rgba.blue*255),
 			(int)(rgbb.rgba.red*255), (int)(rgbb.rgba.green*255), (int)(rgbb.rgba.blue*255), rgbb.rgba.alpha,
-			(int)(bg_color->rgba.red*255), (int)(bg_color->rgba.green*255), (int)(bg_color->rgba.blue*255), bg_color->rgba.alpha);
+			(int)(bg_color.rgba.red*255), (int)(bg_color.rgba.green*255), (int)(bg_color.rgba.blue*255), bg_color.rgba.alpha);
 		
 		// css body: load a custom file if it exists
 		gchar *cCustomCss = NULL;
@@ -144,7 +147,8 @@ void _init_menu_style (void)
 				-unico-glow-color: transparent; \
 				-unico-glow-radius: 0; \
 			} \
-			.gldimenuitem GtkImage { \
+			.gldimenuitem GtkImage, \
+			.gldimenuitem .image { \
 				background: transparent; \
 			} \
 			.gldimenuitem.separator, \
@@ -171,7 +175,7 @@ void _init_menu_style (void)
 				border-color: @menuitem_child_bg_color; \
 				-unico-inner-stroke-color: transparent; \
 			} \
-			.gldimenuitem *:insensitive { \
+			.gldimenuitem *:disabled { \
 				text-shadow: none; \
 				color: @menuitem_insensitive_text_color; \
 				background: transparent; \
@@ -244,7 +248,7 @@ void _init_menu_style (void)
 				background-color: @menuitem_child_bg_color; \
 				background-image: none; \
 			} \
-			.gldimenuitem GtkCalendar:inconsistent { \
+			.gldimenuitem GtkCalendar:indeterminate { \
 				color: shade (@menuitem_child_bg_color, 0.6); \
 			} \
 			.gldimenuitem .toolbar .button, \
@@ -515,27 +519,6 @@ void gldi_menu_init (GtkWidget *pMenu, Icon *pIcon)
 				"deactivate",
 				G_CALLBACK (_on_menu_deactivated),
 				NULL);
-			
-			gchar *css = NULL;
-			const gchar *orientation = "";
-			int ah = pParams->iArrowHeight;
-			switch (pParams->iMarginPosition)
-			{
-				case 0: orientation = "bottom";  break;
-				case 1: orientation = "top";  break;
-				case 2: orientation = "right";  break;
-				case 3: orientation = "left";  break;
-				default: break;
-			}
-			
-			GtkCssProvider *cssProvider = gtk_css_provider_new ();
-			css = g_strdup_printf ("GtkMenu { \
-				padding-%s: %dpx; \
-			}", orientation, ah);
-			gtk_css_provider_load_from_data (cssProvider,
-				css, -1, NULL);
-			gtk_style_context_add_provider (gtk_widget_get_style_context(pMenu), GTK_STYLE_PROVIDER(cssProvider), GTK_STYLE_PROVIDER_PRIORITY_USER);
-			g_free (css);
 		}
 	}
 }
@@ -612,23 +595,11 @@ static void _init_menu_item (GtkWidget *pMenuItem)
 	gboolean bStyleIsSet = GPOINTER_TO_INT (g_object_get_data (G_OBJECT (pMenuItem), "gldi-style-set"));
 	if (! bStyleIsSet)  // not done yet -> do it once
 	{
-		// the following code is to draw the menu item; this is more like a proof of concept
-		/**g_signal_connect (G_OBJECT (pMenuItem),
+		// draw the menu items; actually, we only want to draw the separators, which are not well drawn by GTK
+		g_signal_connect (G_OBJECT (pMenuItem),
 			"draw",
 			G_CALLBACK (_draw_menu_item),
 			NULL);
-		g_signal_connect (G_OBJECT (pMenuItem),
-			"select",
-			G_CALLBACK (_on_select_menu_item),
-			NULL);
-		g_signal_connect (G_OBJECT (pMenuItem),
-			"deselect",
-			G_CALLBACK (_on_deselect_menu_item),
-			NULL);
-		g_signal_connect (G_OBJECT (pMenuItem),
-			"destroy",
-			G_CALLBACK (_on_destroy_menu_item),
-			NULL);*/
 		
 		gtk_style_context_add_class (gtk_widget_get_style_context (pMenuItem), "gldimenuitem");
 		
@@ -707,212 +678,52 @@ void gldi_menu_popup (GtkWidget *menu)
  /// MENU ITEM ///
 /////////////////
 
-const int N = 10;
-const int dt1 = 20;
-const int dt2 = 30;
-
-/*
-static void
-get_arrow_size (GtkWidget *widget,
-                GtkWidget *child,
-                gint      *size,
-                gint      *spacing)
-{
-	PangoContext     *context;
-	PangoFontMetrics *metrics;
-	gfloat            arrow_scaling;
-	gint              arrow_spacing;
-
-	g_assert (size);
-
-	gtk_widget_style_get (widget,
-		"arrow-scaling", &arrow_scaling,
-		"arrow-spacing", &arrow_spacing,
-		NULL);
-
-	if (spacing != NULL)
-		*spacing = arrow_spacing;
-
-	context = gtk_widget_get_pango_context (child);
-
-	metrics = pango_context_get_metrics (context,
-	                                     pango_context_get_font_description (context),
-	                                     pango_context_get_language (context));
-
-	*size = (PANGO_PIXELS (pango_font_metrics_get_ascent (metrics) +
-	                       pango_font_metrics_get_descent (metrics)));
-
-	pango_font_metrics_unref (metrics);
-
-	*size = *size * arrow_scaling;
-}
 
 static gboolean _draw_menu_item (GtkWidget *widget,
 	cairo_t *cr,
 	G_GNUC_UNUSED gpointer data)
 {
-	//GtkStateFlags state;
-	GtkStyleContext *context;
-	GtkWidget *child;
-	gint x, y, w, h, width, height;
+	GtkWidget *child = gtk_bin_get_child (GTK_BIN (widget));
+	if (child)  // not a separator => skip drawing anything and let GTK handle it
+		return FALSE;
+	
+	// get menu's geometry
 	guint border_width = gtk_container_get_border_width (GTK_CONTAINER (widget));
-	
-	child = gtk_bin_get_child (GTK_BIN (widget));
-	
-	//state = gtk_widget_get_state_flags (widget);
-	context = gtk_widget_get_style_context (widget);
-	width = gtk_widget_get_allocated_width (widget);
-	height = gtk_widget_get_allocated_height (widget);
+	gint width = gtk_widget_get_allocated_width (widget);
+	gint height = gtk_widget_get_allocated_height (widget);
 
+	gint x, y, w, h;
 	x = border_width;
 	y = border_width;
 	w = width - border_width * 2;
 	h = height - border_width * 2;
 
-	// draw the background
-	int n = GPOINTER_TO_INT (g_object_get_data (G_OBJECT (widget), "gldi-step"));
-	if (n != 0)  // so we only draw the barkground if the item is or was selected
-	{
-		double a = (double)n/N;
-
-		cairo_save (cr);
-
-		int r=6, l=0;
-		cairo_dock_draw_rounded_rectangle (cr, r, l, w - 2*r, h);
-		cairo_clip (cr);
-
-		gldi_style_colors_set_selected_bg_color (cr);
-		cairo_paint_with_alpha (cr, a);
-
-		cairo_restore (cr);
-	}
+	// get the line color of the menu
+	GldiColor rgb;
+	if (myDialogsParam.bUseDefaultColors)
+		gldi_style_color_get (GLDI_COLOR_LINE, &rgb);
+	else
+		rgb = myDialogsParam.fLineColor;
 	
-	// draw the arrow in case of a sub-menu
-	if (gtk_menu_item_get_submenu (GTK_MENU_ITEM (widget)))
-	{
-		gint arrow_x, arrow_y;
-		gint arrow_size, spacing;
-		GtkTextDirection direction;
-		gdouble angle;
-
-		direction = gtk_widget_get_direction (widget);
-		get_arrow_size (widget, child, &arrow_size, &spacing);
-
-		if (direction == GTK_TEXT_DIR_LTR)
-		{
-		  arrow_x = x + w - arrow_size - spacing;
-		  angle = G_PI / 2;
-		}
-		else
-		{
-		  arrow_x = x + spacing;
-		  angle = (3 * G_PI) / 2;
-		}
-
-		arrow_y = y + (h - arrow_size) / 2;
-
-		gtk_render_arrow (context, cr, angle, arrow_x, arrow_y, arrow_size);
-	}
-	else if (!child)  // separator
-	{
-		gboolean wide_separators;
-		gint     separator_height;
-		GtkBorder padding;
-
-		gtk_style_context_get_padding (context, 0, &padding);
-		gtk_widget_style_get (widget,
-			"wide-separators",    &wide_separators,
-			"separator-height",   &separator_height,
-			NULL);
-		if (wide_separators)
-			gtk_render_frame (context, cr,
-				x + padding.left,
-				y + padding.top,
-				w - padding.left - padding.right,
-				separator_height);
-		else
-			gtk_render_line (context, cr,
-				x + padding.left,
-				y + padding.top,
-				x + w - padding.right - 1,
-				y + padding.top);
-	}
+	// make a pattern with the alpha channel: 0 - 0.1 ---- .9 - 1
+	int mb = w*.05;  // margin to border
+	cairo_pattern_t *pattern;
+	pattern = cairo_pattern_create_linear (x+mb, y, x+w-mb, y);
+	cairo_pattern_add_color_stop_rgba (pattern, 0., rgb.rgba.red, rgb.rgba.green, rgb.rgba.blue, rgb.rgba.alpha*.1);
+	cairo_pattern_add_color_stop_rgba (pattern, .1, rgb.rgba.red, rgb.rgba.green, rgb.rgba.blue, rgb.rgba.alpha);
+	cairo_pattern_add_color_stop_rgba (pattern, .9, rgb.rgba.red, rgb.rgba.green, rgb.rgba.blue, rgb.rgba.alpha);
+	cairo_pattern_add_color_stop_rgba (pattern, 1., rgb.rgba.red, rgb.rgba.green, rgb.rgba.blue, rgb.rgba.alpha*.1);
+	cairo_set_source (cr, pattern);
 	
-	// draw the item's content
-	GtkWidgetClass *parent_class = g_type_class_peek (g_type_parent (G_TYPE_FROM_INSTANCE (widget)));
-	parent_class = g_type_class_peek_parent (parent_class);
+	// draw the separator as a 1px line with a margin from the border
+	cairo_move_to(cr, x+mb, y);
+	cairo_set_line_width (cr, 1);
+	cairo_line_to(cr, x+w-mb, y);
+	cairo_stroke(cr);
+	cairo_pattern_destroy (pattern);
 	
-	cairo_set_source_rgba (cr, 1, 1, 1, 1);
-	
-	parent_class->draw (widget, cr);
 	return TRUE;  // intercept
 }
-
-static gboolean _update_menu_item (GtkWidget* pMenuItem, G_GNUC_UNUSED gpointer data)
-{
-	int n = GPOINTER_TO_INT (g_object_get_data (G_OBJECT(pMenuItem), "gldi-step"));
-	gboolean bInside = GPOINTER_TO_INT (g_object_get_data (G_OBJECT(pMenuItem), "gldi-inside"));
-	
-	if (bInside)
-	{
-		if (n < N)
-			n ++;
-	}
-	else
-	{
-		if (n > 0)
-			n --;
-	}
-	g_object_set_data (G_OBJECT(pMenuItem), "gldi-step", GINT_TO_POINTER (n));
-	
-	gtk_widget_queue_draw (pMenuItem);
-	
-	if (n == 0 || n == N)
-	{
-		guint iSidAnimation = GPOINTER_TO_UINT (g_object_get_data (G_OBJECT(pMenuItem), "gldi-animation"));
-		if (iSidAnimation != 0)
-		{
-			g_source_remove (iSidAnimation);
-			g_object_set_data (G_OBJECT(pMenuItem), "gldi-animation", NULL);
-		}
-		return FALSE;
-	}
-	return TRUE;
-}
-static gboolean _on_select_menu_item (GtkWidget* pMenuItem, G_GNUC_UNUSED gpointer data)
-{
-	guint iSidAnimation = GPOINTER_TO_UINT (g_object_get_data (G_OBJECT(pMenuItem), "gldi-animation"));
-	if (iSidAnimation != 0)
-		g_source_remove (iSidAnimation);
-	iSidAnimation = g_timeout_add (dt1, (GSourceFunc)_update_menu_item, pMenuItem);
-	g_object_set_data (G_OBJECT(pMenuItem), "gldi-animation", GUINT_TO_POINTER (iSidAnimation));
-	
-	g_object_set_data (G_OBJECT(pMenuItem), "gldi-inside", GINT_TO_POINTER (TRUE));
-	
-	return FALSE;
-}
-static gboolean _on_deselect_menu_item (GtkWidget* pMenuItem, G_GNUC_UNUSED gpointer data)
-{
-	guint iSidAnimation = GPOINTER_TO_UINT (g_object_get_data (G_OBJECT(pMenuItem), "gldi-animation"));
-	if (iSidAnimation != 0)
-		g_source_remove (iSidAnimation);
-	iSidAnimation = g_timeout_add (dt2, (GSourceFunc)_update_menu_item, pMenuItem);
-	g_object_set_data (G_OBJECT(pMenuItem), "gldi-animation", GUINT_TO_POINTER (iSidAnimation));
-	
-	g_object_set_data (G_OBJECT(pMenuItem), "gldi-inside", GINT_TO_POINTER (FALSE));
-	
-	return FALSE;
-}
-static void _on_destroy_menu_item (GtkWidget* pMenuItem, G_GNUC_UNUSED gpointer data)
-{
-	guint iSidAnimation = GPOINTER_TO_UINT (g_object_get_data (G_OBJECT(pMenuItem), "gldi-animation"));
-	if (iSidAnimation != 0)
-	{
-		g_source_remove (iSidAnimation);
-		g_object_set_data (G_OBJECT(pMenuItem), "gldi-animation", NULL);
-	}
-}
-*/
 
 GtkWidget *gldi_menu_item_new_full (const gchar *cLabel, const gchar *cImage, gboolean bUseMnemonic, GtkIconSize iSize)
 {
