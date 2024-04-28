a2542d1045feb8166dbe77ef1d4d82e5465d987d
Mamoru TASAKA <mtasaka@fedoraproject.org>
2024-04-28 13:15:50 +0900

    Help: initialize pCategoryCombo before used
    cairo_dock_show_tips() calls _cairo_dock_get_next_tip(pTips) ,
    which uses pTips->pCategoryCombo , so this must be properly
    created (initialized) before calling _cairo_dock_get_next_tip(pTips).

--- Help/src/applet-tips-dialog.c
+++ Help/src/applet-tips-dialog.c
@@ -287,10 +287,6 @@ void cairo_dock_show_tips (void)
 	pTips->iNumTipGroup = iNumTipGroup;
 	pTips->iNumTipKey = iNumTipKey;
 	
-	// update to the next tip.
-	if (myData.iLastTipGroup >= 0 && myData.iLastTipKey >= 0)  // a previous tip exist => take the next one;
-		_cairo_dock_get_next_tip (pTips);
-	
 	// build a list of the available groups.
 	GtkWidget *pInteractiveWidget = gtk_box_new (GTK_ORIENTATION_VERTICAL, 3);
 	GtkWidget *pComboBox = gtk_combo_box_text_new ();
@@ -301,6 +297,11 @@ void cairo_dock_show_tips (void)
 	}
 	gtk_combo_box_set_active (GTK_COMBO_BOX (pComboBox), pTips->iNumTipGroup);
 	pTips->pCategoryCombo = pComboBox;
+
+	// update to the next tip.
+	if (myData.iLastTipGroup >= 0 && myData.iLastTipKey >= 0)  // a previous tip exist => take the next one;
+		_cairo_dock_get_next_tip (pTips);
+	
 	static gpointer data_combo[2];
 	data_combo[0] = pTips;  // the 2nd data is the dialog, we'll set it after we make it.
 	g_signal_connect (G_OBJECT (pComboBox), "changed", G_CALLBACK(_on_tips_category_changed), data_combo);
