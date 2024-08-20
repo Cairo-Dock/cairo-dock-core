Cairo-Dock/cairo-dock-core

commit d07d2126542bda1f02f69e136f5e63da88b69d4e

fix warnings related to g_string_free ()

src/gldit/cairo-dock-class-manager.c
@@ -1677,17 +1677,9 @@ static gchar *_search_desktop_file (const gchar *cDesktopFile)  // file, path or
 	g_free (cFileNameLower);
 	g_free (cDesktopFileName);
 
-	gchar *cResult;
-	if (bFound)
-	{
-		cResult = sDesktopFilePath->str;
-		g_string_free (sDesktopFilePath, FALSE);
-	}
-	else
-	{
-		cResult = NULL;
-		g_string_free (sDesktopFilePath, TRUE);
-	}
+	gchar *cResult = NULL;
+	if (bFound) cResult = g_string_free_and_steal (sDesktopFilePath);
+	else g_string_free (sDesktopFilePath, TRUE);
 	return cResult;
 }
 

src/gldit/cairo-dock-dock-manager.c
@@ -287,9 +287,7 @@ gchar *cairo_dock_get_unique_dock_name (const gchar *cPrefix)
 		i ++;
 	}
 	
-	gchar *cUniqueName = sNameString->str;
-	g_string_free (sNameString, FALSE);
-	return cUniqueName;
+	return g_string_free_and_steal (sNameString);
 }
 
 gboolean cairo_dock_check_unique_subdock_name (Icon *pIcon)

src/gldit/cairo-dock-icon-manager.c
@@ -355,9 +355,7 @@ gchar *cairo_dock_search_icon_s_path (const gchar *cFileName, gint iDesiredIconS
 		return NULL;
 	}
 	
-	gchar *cIconPath = sIconPath->str;
-	g_string_free (sIconPath, FALSE);
-	return cIconPath;
+	return g_string_free_and_steal (sIconPath);
 }
 
 void cairo_dock_add_path_to_icon_theme (const gchar *cThemePath)

src/gldit/cairo-dock-module-manager.c
@@ -113,9 +113,7 @@ static gchar *_gldi_module_list_active (void)
 	if (pString->len > 0)
 		pString->str[pString->len-1] = '\0';
 	
-	gchar *cModuleNames = pString->str;
-	g_string_free (pString, FALSE);
-	return cModuleNames;
+	return g_string_free_and_steal (pString);
 }
 
 static gboolean _write_modules_idle (G_GNUC_UNUSED gpointer data)

src/gldit/cairo-dock-packages.c
@@ -355,12 +355,7 @@ gchar *cairo_dock_get_url_data_with_post (const gchar *cURL, gboolean bGetOutput
 	
 	//\_______________ On recupere les donnees.
 	gchar *cContent = NULL;
-	if (buffer != NULL)
-	{
-		cContent = buffer->str;
-		g_string_free (buffer, FALSE);
-	}
-	
+	if (buffer != NULL) cContent = g_string_free_and_steal (buffer);
 	return cContent;
 }
 
@@ -417,12 +412,7 @@ gchar *cairo_dock_get_url_data_with_headers (const gchar *cURL, gboolean bGetOut
 	
 	//\_______________ On recupere les donnees.
 	gchar *cContent = NULL;
-	if (buffer != NULL)
-	{
-		cContent = buffer->str;
-		g_string_free (buffer, FALSE);
-	}
-	
+	if (buffer != NULL) cContent = g_string_free_and_steal (buffer);
 	return cContent;
 }
 

commit 7561cb09105cad9ff7b86bee12fe8cb15184174d

Fix compilation with older Glib version
Issue introduced by commit d07d212 as g_string_free_and_steal() is only available for Glib version > 2.76. Provide our own wrapper macro instead.

src/gldit/cairo-dock-class-manager.c
@@ -28,7 +28,7 @@
 #include "cairo-dock-icon-facility.h"
 #include "cairo-dock-surface-factory.h"
 #include "cairo-dock-log.h"
-#include "cairo-dock-utils.h"  // cairo_dock_remove_version_from_string
+#include "cairo-dock-utils.h"  // cairo_dock_remove_version_from_string, gldi_string_free_and_steal
 #include "cairo-dock-dock-manager.h"
 #include "cairo-dock-applet-manager.h"
 #include "cairo-dock-launcher-manager.h"
@@ -1678,7 +1678,7 @@ static gchar *_search_desktop_file (const gchar *cDesktopFile)  // file, path or
 	g_free (cDesktopFileName);
 
 	gchar *cResult = NULL;
-	if (bFound) cResult = g_string_free_and_steal (sDesktopFilePath);
+	if (bFound) cResult = gldi_string_free_and_steal (sDesktopFilePath);
 	else g_string_free (sDesktopFilePath, TRUE);
 	return cResult;
 }

src/gldit/cairo-dock-dock-manager.c
@@ -56,6 +56,7 @@
 #include "cairo-dock-dock-visibility.h"
 #include "cairo-dock-dock-manager.h"
 #include "cairo-dock-windows-manager.h"
+#include "cairo-dock-utils.h" // gldi_string_free_and_steal
 
 
 // public (manager, config, data)
@@ -287,7 +288,7 @@ gchar *cairo_dock_get_unique_dock_name (const gchar *cPrefix)
 		i ++;
 	}
 	
-	return g_string_free_and_steal (sNameString);
+	return gldi_string_free_and_steal (sNameString);
 }
 
 gboolean cairo_dock_check_unique_subdock_name (Icon *pIcon)

src/gldit/cairo-dock-icon-manager.c
@@ -49,6 +49,7 @@
 #include "cairo-dock-applet-manager.h"  // GLDI_OBJECT_IS_APPLET_ICON
 #include "cairo-dock-backends-manager.h"  // cairo_dock_foreach_icon_container_renderer
 #include "cairo-dock-style-manager.h"
+#include "cairo-dock-utils.h" // gldi_string_free_and_steal
 #define _MANAGER_DEF_
 #include "cairo-dock-icon-manager.h"
 
@@ -355,7 +356,7 @@ gchar *cairo_dock_search_icon_s_path (const gchar *cFileName, gint iDesiredIconS
 		return NULL;
 	}
 	
-	return g_string_free_and_steal (sIconPath);
+	return gldi_string_free_and_steal (sIconPath);
 }
 
 void cairo_dock_add_path_to_icon_theme (const gchar *cThemePath)

src/gldit/cairo-dock-module-manager.c
@@ -34,6 +34,7 @@
 #include "cairo-dock-animations.h"
 #include "cairo-dock-config.h"
 #include "cairo-dock-module-instance-manager.h"
+#include "cairo-dock-utils.h" // gldi_string_free_and_steal
 #define _MANAGER_DEF_
 #include "cairo-dock-module-manager.h"
 
@@ -113,7 +114,7 @@ static gchar *_gldi_module_list_active (void)
 	if (pString->len > 0)
 		pString->str[pString->len-1] = '\0';
 	
-	return g_string_free_and_steal (pString);
+	return gldi_string_free_and_steal (pString);
 }
 
 static gboolean _write_modules_idle (G_GNUC_UNUSED gpointer data)

src/gldit/cairo-dock-packages.c
@@ -33,6 +33,7 @@
 #include "cairo-dock-task.h"
 #include "cairo-dock-config.h"
 #include "cairo-dock-log.h"
+#include "cairo-dock-utils.h" // gldi_string_free_and_steal
 #define _MANAGER_DEF_
 #include "cairo-dock-packages.h"
 
@@ -355,7 +356,7 @@ gchar *cairo_dock_get_url_data_with_post (const gchar *cURL, gboolean bGetOutput
 	
 	//\_______________ On recupere les donnees.
 	gchar *cContent = NULL;
-	if (buffer != NULL) cContent = g_string_free_and_steal (buffer);
+	if (buffer != NULL) cContent = gldi_string_free_and_steal (buffer);
 	return cContent;
 }
 
@@ -412,7 +413,7 @@ gchar *cairo_dock_get_url_data_with_headers (const gchar *cURL, gboolean bGetOut
 	
 	//\_______________ On recupere les donnees.
 	gchar *cContent = NULL;
-	if (buffer != NULL) cContent = g_string_free_and_steal (buffer);
+	if (buffer != NULL) cContent = gldi_string_free_and_steal (buffer);
 	return cContent;
 }
 

src/gldit/cairo-dock-utils.h
@@ -19,6 +19,9 @@
 
 #ifndef __CAIRO_DOCK_UTILS__
 #define  __CAIRO_DOCK_UTILS__
+
+#include <glib.h> // GLIB_CHECK_VERSION
+
 G_BEGIN_DECLS
 
 /**
@@ -79,6 +82,13 @@ gchar * cairo_dock_get_command_with_right_terminal (const gchar *cCommand);
 #define gldi_strings_differ(s1, s2) (!s1 ? s2 != NULL : !s2 ? s1 != NULL : strcmp(s1, s2) != 0)
 #define cairo_dock_strings_differ gldi_strings_differ
 
+/** provide g_string_free_and_steal () for older glib versions
+ *  (avoids warnings when compiling on newer versions) */
+#if GLIB_CHECK_VERSION(2, 76, 0)
+#define gldi_string_free_and_steal g_string_free_and_steal
+#else
+#define gldi_string_free_and_steal(s1) g_string_free(s1, FALSE)
+#endif
 
 #include "gldi-config.h"
 #ifdef HAVE_X11