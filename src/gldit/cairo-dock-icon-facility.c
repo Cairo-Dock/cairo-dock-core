4c0f69cb79086c2aae011108159d9cd2c54049bf
Daniel Kondor <kondor.dani@gmail.com>
2025-07-05 14:58:15 +0200

    icon-facility: provide a better way for get a scaled cairo surface from an icon
    Create a Cairo surface instead of a GdkPixbuf, which allows us to preserve the device scale factor used.

--- src/gldit/cairo-dock-icon-facility.c
+++ src/gldit/cairo-dock-icon-facility.c
@@ -623,6 +623,13 @@ GdkPixbuf *cairo_dock_icon_buffer_to_pixbuf (Icon *icon)
 	return cairo_dock_image_buffer_to_pixbuf (&icon->image, 24, 24);
 }
 
+cairo_surface_t *cairo_dock_icon_buffer_to_cairo (Icon *icon, int iWidth, int iHeight)
+{
+	g_return_val_if_fail (icon != NULL, NULL);
+	
+	return cairo_dock_image_buffer_copy_scale (&icon->image, iWidth, iHeight);
+}
+
 cairo_t *cairo_dock_begin_draw_icon_cairo (Icon *pIcon, gint iRenderingMode, cairo_t *pCairoContext)
 {
 	/*if (pIcon->pContainer)
