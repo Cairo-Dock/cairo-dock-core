9790eab328a7a6fa845cc554a5d4c97687152f49
Kondor Dániel <kondor.dani@gmail.com>
2025-08-31 15:01:48 +0200

    systemd: remove support for moving an already launched app to its scope
    Apps should be launched by systemd instead using the new API.

--- src/implementations/cairo-dock-systemd-integration.c
+++ src/implementations/cairo-dock-systemd-integration.c
@@ -27,76 +27,6 @@
 
 GDBusProxy *s_proxy = NULL;
 
-static void _child_watch_dummy (GPid pid, gint, gpointer)
-{
-	g_spawn_close_pid (pid); // note: this is a no-op
-}
-
-static void _create_scope_end (GObject*, GAsyncResult *res, gpointer ptr)
-{
-	GError *err = NULL;
-	GVariant *ret = g_dbus_proxy_call_finish (s_proxy, res, &err);
-	if (ret) g_variant_unref (ret);
-	else
-	{
-		cd_warning ("couldn't set scope for child process: %s", err->message);
-		g_error_free (err);
-	}
-	
-	// we only create the child watch after the DBus call to systemd finished
-	// hopefully this avoids pid reuse race conditions (i.e. until it has been
-	// waited for, the pid should become a zombie if the process exits)
-	g_child_watch_add (GPOINTER_TO_INT (ptr), _child_watch_dummy, NULL);
-}
-
-static void _create_transient_scope (const char *id, const char *desc, GPid pid)
-{
-	if (!s_proxy) return;
-	
-	GVariantBuilder var_builder;
-	/*
-	       StartTransientUnit(in  s name,
-                         in  s mode,
-                         in  a(sv) properties,
-                         in  a(sa(sv)) aux,
-                         out o job);
-	*/
-	GVariantType *full_type  = g_variant_type_new ("(ssa(sv)a(sa(sv)))");
-	GVariantType *props_type = g_variant_type_new ("a(sv)");
-	GVariantType *aux_type   = g_variant_type_new ("a(sa(sv))");
-	
-	char *name;
-	size_t len = strlen (id);
-	const size_t max_len =
-		255 // length allowed by systemd
-		- 42 // length of our prefix + dash + suffix
-		- 9; // max length of %d specifier (assuming pid_t is 32 bits)
-	if (len > max_len)
-		name = g_strdup_printf ("cairo-dock-launched-%.*s-%d.scope", (int)max_len, id, pid);
-	else name = g_strdup_printf ("cairo-dock-launched-%s-%d.scope", id, pid);
-	unsigned int tmp = (unsigned int)pid;
-	
-	g_variant_builder_init  (&var_builder, full_type);
-	g_variant_builder_add   (&var_builder, "s", name);
-	g_variant_builder_add   (&var_builder, "s", "fail");
-	g_variant_builder_open  (&var_builder, props_type);
-	g_variant_builder_add   (&var_builder, "(sv)", "Description", g_variant_new_string (desc));
-	g_variant_builder_add   (&var_builder, "(sv)", "PIDs", g_variant_new_fixed_array (G_VARIANT_TYPE_UINT32, &tmp, 1, 4));
-	g_variant_builder_close (&var_builder);
-	g_variant_builder_open  (&var_builder, aux_type);
-	g_variant_builder_close (&var_builder);
-	
-	GVariant *var = g_variant_ref_sink (g_variant_builder_end (&var_builder));
-	g_dbus_proxy_call (s_proxy, "StartTransientUnit", var, G_DBUS_CALL_FLAGS_NONE, -1, NULL, _create_scope_end, GINT_TO_POINTER (pid));
-	
-	g_variant_unref (var);
-	g_free (name);
-	g_variant_type_free (full_type);
-	g_variant_type_free (props_type);
-	g_variant_type_free (aux_type);
-}
-
-
 static guint64 s_iLaunchID = 0;
 
 static void _spawn_end (GObject*, GAsyncResult *res, gpointer)
@@ -207,7 +137,6 @@ static void _proxy_connected (GObject*, GAsyncResult *res, gpointer)
 		}
 		
 		GldiChildProcessManagerBackend backend;
-		backend.new_app_launched = _create_transient_scope;
 		backend.spawn_app = _spawn_app;
 		gldi_register_process_manager_backend (&backend);
 	}
