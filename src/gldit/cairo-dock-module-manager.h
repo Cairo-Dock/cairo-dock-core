a440191e2691e84644439e3f490cfca6642072f6
Kondor Dániel <kondor.dani@gmail.com>
2025-11-29 19:31:36 +0100

    module-manager: bump ABI version

--- src/gldit/cairo-dock-module-manager.h
+++ src/gldit/cairo-dock-module-manager.h
@@ -44,7 +44,7 @@ G_BEGIN_DECLS
  * It is not required the change this when adding a function to the
  * public API (loading the module will fail if it refers to an
  * unresolved symbol anyway). */
-#define GLDI_ABI_VERSION 20251215
+#define GLDI_ABI_VERSION 20251220
 
 // manager
 typedef struct _GldiModulesParam GldiModulesParam;
