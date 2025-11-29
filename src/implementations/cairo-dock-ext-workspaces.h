04deb983002ba69429690da0c38784e530f18eff
Kondor Dániel <kondor.dani@gmail.com>
2025-11-29 19:37:09 +0100

    ext-workspaces: keep track of workspace groups
    Adapt to the new desktop-manager API and fill out the desktop and workspace layout accurately.

--- src/implementations/cairo-dock-ext-workspaces.h
+++ src/implementations/cairo-dock-ext-workspaces.h
@@ -29,8 +29,8 @@ extern struct wl_output *s_ws_output;
 
 gboolean gldi_ext_workspaces_match_protocol (uint32_t id, const char *interface, uint32_t version);
 gboolean gldi_ext_workspaces_try_init (struct wl_registry *registry);
-struct ext_workspace_handle_v1 *gldi_ext_workspaces_get_handle (int x, int y);
-gboolean gldi_ext_workspaces_find (struct ext_workspace_handle_v1 *handle, int *x, int *y);
+struct ext_workspace_handle_v1 *gldi_ext_workspaces_get_handle (int iDesktop, int x, int y);
+gboolean gldi_ext_workspaces_find (struct ext_workspace_handle_v1 *handle, int *iNbDesktop, int *x, int *y);
 
 #endif
 
